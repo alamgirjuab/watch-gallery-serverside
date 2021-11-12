9cc146c613404f1051f3c6a7110ff88b534888ee
alamgirjuab <alamgirjuab@gmail.com>
2021-11-13 03:17:27 +0600

    server connected

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gi8q3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        // console.log('database connected successfully');
+        const database = client.db('watchGallery');
+        const productCollection = database.collection('products');
+
+        // GET API
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+        // POST API
+        app.post('/products', async (req, res) => {
+            const products = req.body;
+            console.log('hit the post api', products);
+
+            const result = await productCollection.insertOne(products);
+            console.log(result);
+            res.json(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Watch Gallery')
